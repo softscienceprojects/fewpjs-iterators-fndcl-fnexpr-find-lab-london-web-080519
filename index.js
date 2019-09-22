af318e2e929d12848461d4980e21dbaa91915cd4
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-09-22 10:50:41 +0100

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr) {
+  let toFind = arr.find(entry => entry.result === "W")
+  if (toFind) {
+    return toFind.year
+  } else {
+    return undefined
+  }
+}
